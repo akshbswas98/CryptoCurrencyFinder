830052702310d7c8dac9454bd9a02d8113c1992d
Akash Biswas <64808720+akshbswas98@users.noreply.github.com>
2022-08-06 05:53:37 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import * as React from 'react';
+import Axios from 'axios';
+import { useState,useEffect}  from 'react';
+import './App.css';
+
+function App() {
+const [search,setSearch] = useState("");
+
+const [crypto,setCrypto] = useState([]);
+  useEffect(() => {
+    Axios.get('https://api.coinstats.app/public/v1/coins?skip=0&limit=200&currency=USD').then( (res) =>{
+      setCrypto(res.data.coins)
+    });
+
+  },[])
+  return (
+    <div className="App">
+       <h1 className="title">Dilip - Cryptocurrency finder</h1>
+      <input type="text" placeholder='search using only small letters...' onChange={(e) => { 
+      setSearch(e.target.value)
+    }} />
+      <table>
+      <thead>
+        <tr>
+          <td>Rank</td>
+          <td>name</td>
+          <td>symbol</td>
+          <td>Marketcap</td>
+          <td>Price</td>
+          <td>Available Supply</td>
+          
+        </tr>
+      </thead>
+        {/*Filtering to check results */}
+        <tbody>
+        {crypto 
+        .filter((val)=> { return val.name.toLowerCase()
+        .includes(search)})
+        .map((val, id)=> {
+          return (
+            <>
+            <tr id="id">
+              <td className="rank">{val.rank}</td>
+              <td className="logo">
+              <a href={val.websiteUrl}>
+                  <img  src={val.icon} alt="logo" width="30px"/>
+                  </a>
+                  <p>{val.name}</p>
+                  </td>
+            <td className="symbol">{val.symbol}</td>
+            <td>$ {Math.round(val.marketCap)} </td>
+            <td>$ {Math.round(val.price)} </td>
+            <td>{val.availableSupply}</td>
+            </tr>
+            </>
+          )
+        })
+        }
+      </tbody>
+    </table>
+    </div>
+  );
+  
+}
+
+export default App;
